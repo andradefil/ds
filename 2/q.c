5b404c833d6318db8394f4a38d0a9ace2cf0309d
Filipe Andrade <filipe.andrade@nubank.com.br>
2024-09-01 19:19:39 -0300

    alloc, free item_q

--- 2/q.c
+++ 2/q.c
@@ -1,9 +1,13 @@
 /** Copyright (c) 2024 Filipe W. V. Andrade
  *
- * Simple Queue Algorithm in C.
- * 
+ * Bounded Buffer Algorithm in C, using a circular queue.
+ *
+ * Each call to push get an ID, and enqueue an item,
+ * limited to a max number of items in queue.
+ * The FIFO order are respected to unqueue the items.
+ * When number of items < MAX, more items can be enqueued.
+ *
  */
-
 #include <stdio.h>
 
 #define MAX_ELEM 3
@@ -11,57 +15,74 @@
 
 int queue[MAX_ELEM];
 int head;
+int tail;
+int sz;
 
 void create();
-void push(int elem, int *head);
-int  pop(int *head);
+void push(int *elem, int *pos, char err[]);
+int  pop(int *head, char err[]);
 int  size();
-void printQueue();
+void printq();
 
 void create() {
   head = START_HEAD;
+  tail = START_HEAD;
+  sz = 0;
 };
 
-int size() {
-  if (head > START_HEAD) {
-    return head;
-  } else {
-    return START_HEAD;
-  }
-};
-
-void printQueue() {
-  printf("%p,%d=>", &head, head);
-  for (int i = START_HEAD ; i < head; i++) {
-    printf("%d,", queue[i]);
+void printq() {
+  printf("%p,sz=%d,h=%d,t=%d=>", &head,sz,head,tail);
+  int h = head;
+  // iterate n number of elements
+  // prints next item, starting from the head;
+  for (int n = 0; n < sz; n++) {
+    // ensure to read right position after reset buffer.
+    if (h == MAX_ELEM) {
+      h = START_HEAD;
+    };
+    printf("%d,", queue[h]);
+    h++;
   };
   printf("\n");
   return;
 };
 
-void push(int elem, int *head) {
-  if (*head < MAX_ELEM) {
-    queue[*head] = elem;
-    *head = *head + 1;
-    printQueue();
+void push(int *elem, int *tail, char err[]) {
+  if (sz < MAX_ELEM) {
+    if (*tail == MAX_ELEM) {
+      *tail = START_HEAD;
+    }
+
+    queue[*tail] = *elem;
+    *tail = *tail + 1;
+    sz = sz + 1;
+    *elem = *elem + 1;
+    printq();
   } else {
-    printf("Queue is full.\n");
+    printf("%s\n", err);
   };
 };
 
-int pop(int *head) {
-  int v = queue[*head];
-  if (*head != START_HEAD) {
-    *head = *head-1;
-    printQueue();
+int pop(int *head, char err[]) {
+  if (sz > START_HEAD) {
+    int v;
+    if (*head == MAX_ELEM) {
+      *head = START_HEAD;
+    }
+
+    v= queue[*head];
+    *head = *head + 1;
+    sz = sz - 1;
+    printq();
+    return v;
   } else {
-    printf("Queue is empty.\n");
+    printf("%s\n", err);
   };
-  return v;
+  return -1;
 };
 
 int main() {
-  int op, s, v = 1;
+  int op, v = 1;
   create();
   do {
     printf("1. push\n");
@@ -73,18 +94,18 @@ int main() {
     scanf("%d",&op);
     switch(op) {
     case 1:
-      scanf("%d",&v);
-      push(v, &head);
+      // increment id if queued, prints err when queue is full.
+      push(&v, &tail, "Queue is full.");
       break;
     case 2:
-      pop(&head);
+      // prints err when queue is empty.
+      pop(&head, "Queue is empty.");
       break;
     case 3:
-      s = size();
-      printf("size=>%d\n", s);
+      printf("size=>%d\n", sz);
       break;
     case 4:
-      printQueue();
+      printq();
       break;
     default:
       break;
